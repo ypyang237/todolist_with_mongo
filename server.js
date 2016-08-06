ec1949f5ef800d03e043fe31fab8234041396ce4
ypyang237 <ypyang237@gmail.com>
2016-08-06 12:11:17 +0800

    sets up server.js

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
 'use strict';
 
-const express = require('express')
+const express    = require('express'),
+      bodyParser = require('body-parser'),
+      app        = express(),
+      PORT       = process.env.PORT || 3000
+      ;
+
+app
+  .use(bodyParser.urlencoded({extended: true}))
+  .use(bodyParser.json())
+  ;
+
+app.use(express.static(__dirname + "/public"));
+
+app.get('*', function(req, res){
+  res.sendFile('./public/index.html',
+    {
+      root  : __dirname
+    });
+});
+
+app.listen(PORT, function() {
+  console.log(`Server listening on port: ${PORT}`);
+});
